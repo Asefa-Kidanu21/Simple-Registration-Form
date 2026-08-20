6eabbfe22aa48d4b90166c983f0c53ef2e5eee0c
Asefa Kidanu <asefakidanu21@gmail.com>
2026-08-20 15:03:28 +0300

    new update

--- Frontend/src/Features/Auth/Services/AuthAPI.js
+++ Frontend/src/Features/Auth/Services/AuthAPI.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 export const createUserApi=async (user)=>{
 
-    const result=await axios.post("http://localhost:3000/api/create",user);
+    const result=await axios.post
+    ("https://simple-registration-form-v8tc.onrender.com/api/create",user);
+
+    return result.data;
 }
+
 
 export const selectUserApi=async ()=>{
 
